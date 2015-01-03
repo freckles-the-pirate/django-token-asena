838ba3fb7ee2fe6dec5fd3e5aaeec983babe2025
Jordan <jordannh@sent.com>
2015-01-02 18:53:48 -0800

    Rearrange admin file.

--- asena/admin.py
+++ asena/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from asena.models import token
 
-@admin.register(token.Token)
+
 class TokenAdmin(admin.ModelAdmin):
     pass
-@admin.register(token.TokenSet)
+
 class TokenSetAdmin(admin.ModelAdmin):
     pass
+
+admin.site.register(token.TokenSet, TokenSetAdmin)
+admin.site.register(token.Token, TokenAdmin)
